995559232cc3f5f443baeb05023d2f211682a18e
Khairus <playsave23@gmail.com>
2025-01-26 22:43:31 +0700

    chore: update app routes to include Pokémon detail, filter, and list components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/pokemon-list/pokemon-list.component').then(
+        (m) => m.PokemonListComponent
+      ),
+  },
+  {
+    path: 'pokemon/:name',
+    loadComponent: () =>
+      import('./features/pokemon-detail/pokemon-detail.component').then(
+        (m) => m.PokemonDetailComponent
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
