b6a05463708f54d2678c5376a9817978abef136e
Khairus <playsave23@gmail.com>
2025-01-26 22:41:32 +0700

    feat: configure ionic angular in app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,18 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, RouteReuseStrategy } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import {
+  IonicRouteStrategy,
+  provideIonicAngular,
+} from '@ionic/angular/standalone';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideIonicAngular({ mode: 'ios' }),
     provideRouter(routes),
     provideHttpClient(),
   ],
